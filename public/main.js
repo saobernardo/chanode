873e08e95e9441f4b59db3ad56c954017487bfbd
saobernardo <bernardo.pinheiro@hotmail.com>
2021-05-27 22:59:25 -0300

    Sockets a recibir conexión. Necesita de añedir chatPage cuando apóslogin

--- public/main.js
+++ public/main.js
@@ -1,1 +1,36 @@
 const socket = io(); //iniciando minha conexão socket
+let username = '';
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+function renderUserList(){}
+
+loginInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+        let name = loginInput.value.trim();
+        if(name != ''){
+            username = name;
+            document.title = 'Chat ('+username+')';
+
+            socket.emit('join-request', username);
+        }
+    }
+});
+
+socket.on('user-ok', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+
+    textInput.focus();
+
+    userList = list;
+    renderUserList();
+});
--- server.js
+++ server.js
@@ -11,7 +11,16 @@ server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public'))); //por qual pasta vai começar a ler os arquivos
 
+let connectedUsers = [];
+
 //cada nova conexão irá rodar o que está na função
+//Todo on é um listener
 io.on('connection', (socket) => {
-    
+    socket.on('join-request', (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+
+        socket.emit('user-ok', connectedUsers);
+    }); 
 });
